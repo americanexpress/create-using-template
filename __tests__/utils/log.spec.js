86692d8f0a300d2cdec8543deaf13572b19422eb
Jonathan Adshead <jonathan.adshead@aexp.com>
2021-12-09 12:26:59 -0700

    test(log): snapshot readability

--- __tests__/utils/log.spec.js
+++ __tests__/utils/log.spec.js
@@ -35,34 +35,50 @@ describe('log functions', () => {
     jest.spyOn(console, 'log').mockImplementation(() => {});
     kleur.enabled = false; // color support for unit tests is flaky, disable color for snapshots
   });
+
   describe('stepBanner', () => {
-    it(`should output the correct string for all ${TOTAL_EXPECTED_STEPS} steps`, () => {
-      for (let i = 1; i <= TOTAL_EXPECTED_STEPS; i += 1) {
+    for (let i = 1; i <= TOTAL_EXPECTED_STEPS; i += 1) {
+      it(`outputs correct string for step ${i}`, () => {
         goToStep(i);
-      }
-      expect(console.log).toHaveBeenCalledTimes(TOTAL_EXPECTED_STEPS * 6);
-      // snapshot all calls all at once
-      console.log.mock.calls.forEach((mockCall) => {
-        expect(mockCall).toMatchSnapshot();
+        expect(console.log.mock.calls.join('\n')).toMatchSnapshot();
       });
+    }
+
+    it('outputs the correct string when not given a templateBanner', () => {
+      goToStep(1);
+      expect(console.log.mock.calls.join('\n')).toMatchInlineSnapshot(`
+        "[2J
+
+         _   _     _               _____                    _       _
+        | | | |___(_)_ __   __ _  |_   _|__ _ __ ___  _ __ | | __ _| |_ ___
+        | | | / __| | '_ \\\\ / _\` |   | |/ _ \\\\ '_ \` _ \\\\| '_ \\\\| |/ _\` | __/ _ \\\\
+        | |_| \\\\__ \\\\ | | | | (_| |   | |  __/ | | | | | |_) | | (_| | ||  __/
+         \\\\___/|___/_|_| |_|\\\\__, |   |_|\\\\___|_| |_| |_| .__/|_|\\\\__,_|\\\\__\\\\___|
+                           |___/                     |_|              vpackageVersionMock
+
+        Step 1 -> Step 2 -> Step 3 -> Step 4 -> Step 5 -> Step 6
+        Load the template
+        ________________________________________________________________________________"
+      `);
     });
 
-    it(`outputs the correct string for all ${TOTAL_EXPECTED_STEPS} steps with a banner`, () => {
-      for (let i = 1; i <= TOTAL_EXPECTED_STEPS; i += 1) {
-        goToStep(i, 'BannerMock');
-      }
-      expect(console.log).toHaveBeenCalledTimes(TOTAL_EXPECTED_STEPS * 7);
-      // snapshot all calls all at once
-      console.log.mock.calls.forEach((mockCall) => {
-        expect(mockCall).toMatchSnapshot();
-      });
+    it('outputs the correct string when given a templateBanner', () => {
+      goToStep(1, 'BannerMock');
+      expect(console.log.mock.calls.join('\n')).toMatchInlineSnapshot(`
+        "[2J
+        BannerMock
+        Generator Version: vpackageVersionMock
+
+        Step 1 -> Step 2 -> Step 3 -> Step 4 -> Step 5 -> Step 6
+        Load the template
+        ________________________________________________________________________________"
+      `);
     });
 
     it('should do nothing if called with an index out of range', () => {
       goToStep(0);
       goToStep(-1);
       goToStep(TOTAL_EXPECTED_STEPS + 1);
-      goToStep(100);
       goToStep('index');
       expect(console.log).toHaveBeenCalledTimes(0);
     });
